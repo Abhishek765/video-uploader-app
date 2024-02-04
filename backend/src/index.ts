906f804c678d38081661bb38515911215804d774
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2024-02-04 19:39:37 +0530

    feat: backend code init

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import { connectDB } from './db/connection';
+import { app } from './app';
+
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+
+//! DB Connection
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`listening on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error(`Failed to connect: error: ${err}`));
+
+//! middlewares
+
+//! Routes
+app.get('/', (req, res) => {
+  res.send('<h2> Hello from Home!!!</h2>');
+});
